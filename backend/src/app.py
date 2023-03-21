225b137e16f4a1df230cb1150f41bf416879d6f7
furnapso <luke.boland95@gmail.com>
2023-03-21 17:04:47 +1000

    basic backend functionality

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,9 +2,20 @@ from os import environ
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from riotwatcher import LolWatcher
 
 app = FastAPI()
+origins = ["http://localhost:3000", "localhost:3000"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 RIOT_API_KEY = environ.get("RIOT_API_KEY")
 lol = LolWatcher(RIOT_API_KEY)
@@ -24,5 +35,12 @@ async def summoner_champions(region: str, summoner_name: str):
     return lol.champion_mastery.by_summoner("oc1", summoner_id)
 
 
+# @app.get("/championImage")
+# async def champion_image(image_id):
+#     return lol.da
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    profile_icons = lol.data_dragon.profile_icons
+    print("Hello")
+    # uvicorn.run(app, host="localhost", port=8000)
