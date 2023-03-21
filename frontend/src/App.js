398e6009ad020458cc2a15646bf741fe384ae1cb
furnapso <luke.boland95@gmail.com>
2023-03-21 18:06:38 +1000

    rebuild frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
-import {
-  ChakraProvider, theme
-} from '@chakra-ui/react';
-import React from 'react';
-import { Champions } from './champions';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Champions></Champions>
-    </ChakraProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
