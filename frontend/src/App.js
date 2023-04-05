e6853285c444054065ef4d484f59624bd22e9743
furnapso <luke.boland95@gmail.com>
2023-04-05 20:59:18 +1000

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,24 @@ import { ChampionContainer } from "./champions.js";
 
 function App() {
   return (
-    <section class="section">
-      <div class="section">
-        <h1 class="title">LoL Nuzlocke</h1>
-        <p class="subtitle">Play the League of Legends Nuzlocke Challenge!</p>
-      </div>
-      <div class="container">
-        <ChampionContainer></ChampionContainer>
-      </div>
-    </section>
+    <div>
+      <nav class="navbar">
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">Home</a>
+          </div>
+        </div>
+      </nav>
+      <section class="section">
+        <div class="section">
+          <h1 class="title">LoL Nuzlocke</h1>
+          <p class="subtitle">Play the League of Legends Nuzlocke Challenge!</p>
+        </div>
+        <div class="container">
+          <ChampionContainer></ChampionContainer>
+        </div>
+      </section>
+    </div>
   );
 }
 
