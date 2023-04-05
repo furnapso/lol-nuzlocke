94cf9c40a684eb0e8465b226ff02f1e0a00eb625
furnapso <luke.boland95@gmail.com>
2023-04-05 20:59:03 +1000

    add version endpoint

--- backend/src/app.py
+++ backend/src/app.py
@@ -43,9 +43,18 @@ async def summoner_champions(region: str, summoner_name: str):
     return lol.champion_mastery.by_summoner(region, summoner_id)
 
 
+@cache
+@app.get("/version")
+async def get_latest_version_endpoint():
+    return get_latest_version()
+
+
+def get_latest_version():
+    return lol.data_dragon.versions_all()[0]
+
+
 def get_champions_for_latest_version():
-    version = lol.data_dragon.versions_all()[0]
-    return lol.data_dragon.champions(version)
+    return lol.data_dragon.champions(get_latest_version())
 
 
 def build_champion_id_map(champions):
