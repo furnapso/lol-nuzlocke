524bcf4d6d8fc939889b09b82e247e7837629167
furnapso <luke.boland95@gmail.com>
2023-04-05 18:02:53 +1000

    add caching support

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,6 +3,10 @@ from os import environ
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
 from riotwatcher import LolWatcher
 
 app = FastAPI()
@@ -26,6 +30,7 @@ async def champions():
     return get_champions_for_latest_version()
 
 
+@cache
 @app.get("/summonerChampions")
 async def summoner_champions(region: str, summoner_name: str):
     summoner = lol.summoner.by_name(region, summoner_name)
@@ -50,9 +55,12 @@ def build_champion_id_map(champions):
     return id_map
 
 
-# @app.get("/championImage")
-# async def champion_image(image_id):
-#     return lol.da
+@app.on_event("startup")
+async def startup():
+    redis = aioredis.from_url(
+        "redis://localhost", encoding="utf8", decode_responses=True
+    )
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
 
 
 if __name__ == "__main__":
