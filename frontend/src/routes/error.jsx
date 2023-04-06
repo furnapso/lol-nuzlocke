f592a850f78561eed6daca729ed20bc00da7cfcb
furnapso <luke.boland95@gmail.com>
2023-04-06 22:14:57 +1000

    add error route

---
+++ frontend/src/routes/error.jsx
@@ -0,0 +1,8 @@
+export default function ErrorPage() {
+    return (
+        <div class='section'>
+            <h1 class='title is-2'>Oh no!</h1>
+            <h2 class='subtitle is-3'>Something has gone wrong. Click <a href='/'>here</a> to go back to the home page.</h2>
+        </div>
+    )
+}
