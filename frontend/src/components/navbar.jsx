7a2970fce1c1f4d5ce495efe730b0dad282aad27
furnapso <luke.boland95@gmail.com>
2023-04-06 22:15:05 +1000

    add rules url to navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,11 +1,16 @@
 export default function Navbar() {
-    return (
-        <nav class="navbar">
-        <div class="navbar-menu">
-          <div class="navbar-start">
-            <a class="navbar-item">Home</a>
-          </div>
+  return (
+    <nav class="navbar">
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="/">
+            Home
+          </a>
+          <a class="navbar-item" href="/rules">
+            Rules
+          </a>
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  );
 }
