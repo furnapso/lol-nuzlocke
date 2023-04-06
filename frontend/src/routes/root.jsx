055c5c3b7438cbdf930f0343972d55602f48dbb5
furnapso <luke.boland95@gmail.com>
2023-04-06 22:15:38 +1000

    update root and rules pages

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -4,18 +4,16 @@ import Navbar from "../components/navbar.jsx";
 
 function Root() {
   return (
-      <div>
-        <Navbar />
-        <section class="section">
-        <div class="section">
-          <h1 class="title">LoL Nuzlocke</h1>
-          <p class="subtitle">Play the League of Legends Nuzlocke Challenge!</p>
-        </div>
-        <div class="container">
-          <Outlet />
-        </div>
-      </section>
+    <div>
+      <Navbar />
+      <div class="container is-max-desktop has-text-centered">
+        <h1 class="title">LoL Nuzlocke</h1>
+        <p class="subtitle">Play the League of Legends Nuzlocke Challenge!</p>
       </div>
+      <div class="section">
+        <Outlet />
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/src/routes/rules.jsx
@@ -0,0 +1,21 @@
+export default function Rules() {
+  return (
+    <div class="section">
+      <h1 class="title">Rules</h1>
+      <p>
+        Adapted from the{" "}
+        <a href="https://bulbapedia.bulbagarden.net/wiki/Nuzlocke_Challenge">
+          Pokemon Nuzlocke challenge
+        </a>
+        , the League of Legends Nuzlocke challenge is a set of rules intended to
+        mix up your gameplay experience. The intent is something similar to{" "}
+        <a href="https://www.ultimate-bravery.net/">Ultimate Bravery.</a>
+      </p>
+      <div class="content">
+        <ul>
+          <li>Blah</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
