1150ad337a319f059df6f0e1f68093b8277e0e71
furnapso <luke.boland95@gmail.com>
2023-06-19 08:11:34 +1000

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <>
+      <h1 className="uk-heading-2xlarge">Hello world!</h1>
+    </>
+  );
+}
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "uikit/dist/css/uikit.min.css";
+import "uikit/dist/js/uikit.js";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
