bb4e534589baeb15d2ac7f473eecbd70f631ff76
furnapso <luke.boland95@gmail.com>
2023-06-19 14:23:16 +1000

    add rollresult

---
+++ frontend/src/components/RollResult.jsx
@@ -0,0 +1,22 @@
+export default function RollResult({ champion, handleWin, handleLoss }) {
+  const style = {
+    filter: champion.enabled ? "grayscale(0)" : "grayscale(1)",
+  };
+  return (
+    <>
+      <div className="uk-container uk-flex uk-flex-middle uk-flex-column">
+        <h3 className="uk-title">{champion.name}</h3>
+        <img src={champion.squarePortraitPath} style={style}></img>
+        <div className="uk-container uk-margin uk-flex uk-flex-center uk-flex-space-between">
+          <button className="uk-button uk-button-primary">Win</button>
+          <button
+            className="uk-button uk-button-danger"
+            onClick={() => handleLoss(champion.name)}
+          >
+            Loss
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
